de431477a0ce72fccd6eb513e766f5e5e21c5a2c
Jose Nilton <jnorejuela@hotmail.com>
2024-05-11 15:26:47 -0500

    firts commit

---
+++ src/miProject/clase.java
@@ -0,0 +1,19 @@
+package miProject;
+
+public class clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// a=8, b=3, c=-5,
+		//2*b+ 3*a -c --->45;
+		
+		int a = 8,  b = 3,  c = -5;
+		int result;
+		
+		result = 2 *3 + 3*8 -5;
+		
+		System.out.print(result);
+	}
+
+}
